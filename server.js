ba393f96425a9356d9cd02f0863be9d874458108
ayoaade <ayoaade@terpmail.umd.edu>
2019-12-05 15:14:32 -0500

    added about to sever.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use(express.static("public" + "About.html"));
 
 app.get("/api", (req, res) => {
   const baseURL =
